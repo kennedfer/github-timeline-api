9f6b29c510eaa96ecddf3d5d858eb07f6297fc04
kennedfer <kenned14santos@gmail.com>
2024-08-28 19:30:12 -0300

    feat: add cors to nest (origin all)

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //Mudar o cors depois
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(3000);
 }
 bootstrap();

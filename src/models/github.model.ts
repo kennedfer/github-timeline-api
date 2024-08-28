102d551adf36a19e10539e12d89cfe200d6946a4
kennedfer <kenned14santos@gmail.com>
2024-08-28 19:32:20 -0300

    feat: change type to class to instantiate new objects with structure

--- src/models/github.model.ts
+++ src/models/github.model.ts
@@ -1,12 +1,31 @@
-export type GithubRepository = {
+export class GithubRepository {
   name: string;
   description: string;
-  date: Date;
-};
+  created_at: Date;
+  updated_at: Date;
+  html_url: string;
+
+  constructor({ name, description, created_at, updated_at, html_url }: any) {
+    this.name = name;
+    this.description = description;
+    created_at = created_at;
+    updated_at = updated_at;
+    this.html_url = html_url;
+  }
+}
 
 export class GithubUserInfo {
   name: string;
+  bio: string;
+  avatar_url: string; //i decide to keep the json pattern
   repositories: GithubRepository[];
 
-  constructor(githubApiResponse: any) {}
+  constructor({ name, bio, avatar_url }: any, repositoriesApi: any) {
+    this.name = name;
+    this.bio = bio;
+    this.avatar_url = avatar_url;
+    this.repositories = repositoriesApi.map(
+      (repoApi: any) => new GithubRepository(repoApi),
+    );
+  }
 }
